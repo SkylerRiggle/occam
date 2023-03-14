88fa25c1b563abc4c92dec5c98b0c0992a56e17c
Skyler Riggle <skyler32702@gmail.com>
2023-03-13 21:36:11 -0500

    Logo Sizing

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,6 +8,7 @@ const Home = () =>
                 <img
                     src={Logo}
                     alt="Logo"
+                    height="80%"
                 />
                 <div>
                     <h1>OCCAM LLC.</h1>
