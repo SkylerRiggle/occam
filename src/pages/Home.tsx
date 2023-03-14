857b43bb17661ea1b68f408a0407cb5fbbcd19c6
Skyler Riggle <skyler32702@gmail.com>
2023-03-13 22:04:44 -0500

    FW Update

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,19 +11,19 @@ const Home = () =>
                     height="80%"
                 />
                 <div>
-                    <h1>OCCAM LLC.</h1>
-                    <h1 className="text-orange">Games. Simply.</h1>
+                    <h1 className="drop-shadow display-1 title-bold">OCCAM LLC.</h1>
+                    <h1 className="text-orange drop-shadow display-1 title-bold">Games. Simply.</h1>
                 </div>
             </div>  
 
             <div className="w-100 h-50 bg-black gray-top-border d-flex justify-content-center align-items-center">
                 <div>
-                    <h4 className="text-orange">ABOUT</h4>
-                    <h5>Our mission is to keep things simple.</h5>
+                    <h1 className="text-orange">ABOUT</h1>
+                    <h3>Our mission is to keep things simple.</h3>
                     <br/>
-                    <h5>It's easy to lose your way in game development.</h5>
+                    <h3>It's easy to lose your way in game development.</h3>
                     <br/>
-                    <h5>We keep games simple by keeping our vision the same.</h5>
+                    <h3>We keep games simple by keeping our vision the same.</h3>
                 </div>
             </div>
         </div>
