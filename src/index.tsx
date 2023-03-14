59829da2bcbe4af7fcb481da9883313dfed6ef99
Skyler Riggle <skyler32702@gmail.com>
2023-03-13 21:06:12 -0500

    Implemented Bootstrap & SASS

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 import Site from './Site';
 
+/** Global CSS */
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
