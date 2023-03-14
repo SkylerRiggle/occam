46c2576ac10f9633e86f16fe28ff800104443f08
Skyler Riggle <skyler32702@gmail.com>
2023-03-13 21:29:17 -0500

    Positioning

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ const Home = () =>
 {
     return (
         <div className="vw-100 vh-100">
-            <div className="w-100 h-50 d-flex">
+            <div className="w-100 h-50 d-flex justify-content-center align-items-center">
                 <img
                     src=""
                     alt="Logo"
@@ -13,13 +13,15 @@ const Home = () =>
                 </div>
             </div>  
 
-            <div className="w-100 h-50 bg-black gray-top-border">
-                <h4 className="text-orange">ABOUT</h4>
-                <h5>Our mission is to keep things simple.</h5>
-                <br/>
-                <h5>It's easy to lose your way in game development.</h5>
-                <br/>
-                <h5>We keep games simple by keeping our vision the same.</h5>
+            <div className="w-100 h-50 bg-black gray-top-border d-flex justify-content-center align-items-center">
+                <div>
+                    <h4 className="text-orange">ABOUT</h4>
+                    <h5>Our mission is to keep things simple.</h5>
+                    <br/>
+                    <h5>It's easy to lose your way in game development.</h5>
+                    <br/>
+                    <h5>We keep games simple by keeping our vision the same.</h5>
+                </div>
             </div>
         </div>
     );
